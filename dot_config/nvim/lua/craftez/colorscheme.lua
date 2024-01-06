3775bfdf201679d1959ab740de4a434da71fbd1c
Eduardo Zamora <eduzambl@gmail.com>
2024-01-05 23:00:59 -0600

    feat update based conf

--- dot_config/nvim/lua/craftez/colorscheme.lua
+++ dot_config/nvim/lua/craftez/colorscheme.lua
@@ -1,11 +1,12 @@
 local M = {
-  "LunarVim/primer.nvim",
+  -- "LunarVim/primer.nvim",
+  "rebelot/kanagawa.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "primer_dark"
+  vim.cmd.colorscheme "kanagawa" -- "primer_dark"
 end
 
 return M
