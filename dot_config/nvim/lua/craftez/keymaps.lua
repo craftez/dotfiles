2e5b457a171ef7ea51540ddff888bfdd7a1431de
Eduardo Zamora <eduzambl@gmail.com>
2024-01-08 10:22:05 -0600

    feat: update nvim

--- dot_config/nvim/lua/craftez/keymaps.lua
+++ dot_config/nvim/lua/craftez/keymaps.lua
@@ -8,6 +8,8 @@ vim.g.maplocaleader = " "
 keymap("n", "<C-Space>", "<cmd>WhichKey \\<space><cr>", opts)
 keymap("n", "<C-i>", "<C-i>", opts)
 
+
+
 -- Better window navigation
 keymap("n", "<m-h>", "<C-w>h", opts)
 keymap("n", "<m-j>", "<C-w>h", opts)
@@ -15,18 +17,30 @@ keymap("n", "<m-k>", "<C-w>h", opts)
 keymap("n", "<m-l>", "<C-w>h", opts)
 keymap("n", "<m-tab>", "<c-6>", opts)
 
-keymap("n", "n", "nzz", opts)
-keymap("n", "N", "Nzz", opts)
+-- Better viewing
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
 keymap("n", "*", "*zz", opts)
 keymap("n", "#", "#zz", opts)
 keymap("n", "g*", "g*zz", opts)
 keymap("n", "g#", "g#zz", opts)
+keymap("n", "g,", "g,zvzz")
+keymap("n", "g;", "g;zvzz")
 
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
-keymap("x", "p", [["_dP]])
+-- Paste over currently selected text without yanking it
+keymap("v", "p", [["_dP]])
+
+-- Move lines
+keymap("n", "<A-j>", ":m .+1<CR>==")
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv")
+keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+keymap("n", "<A-k>", ":m .-2<CR>==")
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv")
+keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
 
 vim.cmd [[:amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definition()<CR>]]
 vim.cmd [[:amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]]
