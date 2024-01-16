54996a92e633e8677f1945e29e22daef4fa728a2
Eduardo Zamora <eduzambl@gmail.com>
2024-01-16 09:10:59 -0600

    feat updates config

---
+++ dot_config/NeoVim/lua/plugins/extras.lua
@@ -0,0 +1,260 @@
+return {
+  -- Autotags
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {},
+  },
+
+  -- delete buffer
+  {
+    "famiu/bufdelete.nvim",
+    event = "VeryLazy",
+    config = function()
+      vim.keymap.set(
+        "n",
+        "Q",
+        ":lua require('bufdelete').bufdelete(0, false)<cr>",
+        { noremap = true, silent = true, desc = "Delete buffer" }
+      )
+    end,
+  },
+
+  -- comments
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+    lazy = false,
+  },
+  -- useful when there are embedded languages in certain types of files (e.g. Vue or React)
+  { "joosepalviste/nvim-ts-context-commentstring", lazy = true },
+
+  -- Neovim plugin to improve the default vim.ui interfaces
+  {
+    "stevearc/dressing.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+    config = function()
+      require("dressing").setup()
+    end,
+  },
+
+  -- Neovim notifications and LSP progress messages
+  {
+    "j-hui/fidget.nvim",
+    branch = "legacy",
+    enabled = false,
+    config = function()
+      require("fidget").setup({
+        window = { blend = 0 },
+      })
+    end,
+  },
+
+  -- Smooth scrolling neovim plugin written in lua
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      vim.cmd([[
+        nnoremap <C-j> <C-D>
+        vnoremap <C-j> <C-D>
+
+        nnoremap <C-k> <C-U>
+        vnoremap <C-k> <C-U>
+      ]])
+
+      require("neoscroll").setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb", "C-k", "C-j" },
+        hide_cursor = true,      -- Hide cursor while scrolling
+        stop_eof = true,         -- Stop at <EOF> when scrolling downwards
+        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,   -- Default easing function
+        pre_hook = nil,          -- Function to run before the scrolling animation starts
+        post_hook = nil,         -- Function to run after the scrolling animation ends
+        performance_mode = false, -- Disable "Performance Mode" on all buffers.
+      })
+
+      local t = {}
+      -- Syntax: t[keys] = {function, {function arguments}}
+      t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
+      t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
+      t["<C-k>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
+      t["<C-j>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
+      t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "450" } }
+      t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "450" } }
+      t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
+      t["<C-e>"] = { "scroll", { "0.10", "false", "100" } }
+      t["zt"] = { "zt", { "250" } }
+      t["zz"] = { "zz", { "250" } }
+      t["zb"] = { "zb", { "250" } }
+
+      require("neoscroll.config").set_mappings(t)
+    end,
+  },
+
+  -- find and replace
+  {
+    "windwp/nvim-spectre",
+    event = "BufRead",
+  },
+
+  -- Add/change/delete surrounding delimiter pairs with ease
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+
+  -- Heuristically set buffer options
+  {
+    "tpope/vim-sleuth",
+  },
+
+  -- Neovim setup for init.lua and plugin development with full signature help, docs and completion for the nvim lua API
+  {
+    "folke/neodev.nvim",
+    config = function()
+      require("neodev").setup({
+        library = { plugins = { "neotest" }, types = true },
+      })
+    end,
+  },
+
+  -- Neovim Lua plugin to automatically manage character pairs. Part of 'mini.nvim' library.
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("mini.pairs").setup(opts)
+    end,
+  },
+
+  -- Lorem Ipsum generator for Neovim
+  {
+    "derektata/lorem.nvim",
+    config = function()
+      local lorem = require("lorem")
+      lorem.setup({
+        sentenceLength = "mixedShort",
+        comma = 1,
+      })
+    end,
+  },
+
+  -- Indent guide for Neovim
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    enabled = false,
+    version = "2.1.0",
+    opts = {
+      char = "┊",
+      -- char = "│",
+      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+      show_trailing_blankline_indent = false,
+      show_current_context = false,
+    },
+  },
+
+  -- editor config support
+  {
+    "editorconfig/editorconfig-vim",
+  },
+
+  -- Enhanced f/t motions for Leap
+  {
+    "ggandor/flit.nvim",
+    keys = function()
+      ---@type LazyKeys[]
+      local ret = {}
+      for _, key in ipairs({ "f", "F", "t", "T" }) do
+        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+      end
+      return ret
+    end,
+    opts = { labeled_modes = "nx" },
+  },
+  -- mouse replacement
+  {
+    "ggandor/leap.nvim",
+    keys = {
+      { "s",  mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S",  mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+    end,
+  },
+
+  -- breadcrumbs
+  {
+    "LunarVim/breadcrumbs.nvim",
+    config = function()
+      require("breadcrumbs").setup()
+    end,
+  },
+  -- Simple winbar/statusline plugin that shows your current code context
+  {
+    "SmiteshP/nvim-navic",
+    config = function()
+      local icons = require("config.icons")
+      require("nvim-navic").setup({
+        highlight = true,
+        lsp = {
+          auto_attach = true,
+          preference = { "typescript-tools" },
+        },
+        click = true,
+        separator = " " .. icons.ui.ChevronRight .. " ",
+        depth_limit = 0,
+        depth_limit_indicator = "..",
+        icons = {
+          File = " ",
+          Module = " ",
+          Namespace = " ",
+          Package = " ",
+          Class = " ",
+          Method = " ",
+          Property = " ",
+          Field = " ",
+          Constructor = " ",
+          Enum = " ",
+          Interface = " ",
+          Function = " ",
+          Variable = " ",
+          Constant = " ",
+          String = " ",
+          Number = " ",
+          Boolean = " ",
+          Array = " ",
+          Object = " ",
+          Key = " ",
+          Null = " ",
+          EnumMember = " ",
+          Struct = " ",
+          Event = " ",
+          Operator = " ",
+          TypeParameter = " ",
+        },
+      })
+    end,
+  },
+
+  -- persist sessions
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {},
+  },
+}
