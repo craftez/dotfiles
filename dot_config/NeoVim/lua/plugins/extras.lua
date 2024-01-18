830181ea1a73c4a5405ad30ef4237784048b6c23
Eduardo Zamora <eduzambl@gmail.com>
2024-01-18 08:31:51 -0600

    feat: update neovim quickfix list

--- dot_config/NeoVim/lua/plugins/extras.lua
+++ dot_config/NeoVim/lua/plugins/extras.lua
@@ -97,6 +97,9 @@ return {
   {
     "windwp/nvim-spectre",
     event = "BufRead",
+    keys = {
+      { "<leader>sr", function() require("spectre").open() end, desc = "Search and Replace (Spectre)" }
+    }
   },
 
   -- Add/change/delete surrounding delimiter pairs with ease
@@ -163,38 +166,6 @@ return {
   -- editor config support
   {
     "editorconfig/editorconfig-vim",
-  },
-
-  -- Enhanced f/t motions for Leap
-  {
-    "ggandor/flit.nvim",
-    keys = function()
-      ---@type LazyKeys[]
-      local ret = {}
-      for _, key in ipairs({ "f", "F", "t", "T" }) do
-        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-      end
-      return ret
-    end,
-    opts = { labeled_modes = "nx" },
-  },
-  -- mouse replacement
-  {
-    "ggandor/leap.nvim",
-    keys = {
-      { "s",  mode = { "n", "x", "o" }, desc = "Leap forward to" },
-      { "S",  mode = { "n", "x", "o" }, desc = "Leap backward to" },
-      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-    },
-    config = function(_, opts)
-      local leap = require("leap")
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
-    end,
   },
 
   -- breadcrumbs
