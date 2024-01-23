936b86b7e95d8687a9ab994513a57fb657f7ae2c
Eduardo Zamora <eduzambl@gmail.com>
2024-01-22 19:50:40 -0600

    feat: update nvim

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,50 @@
 return {
-  -- add gruvbox
-  "ellisonleao/gruvbox.nvim",
-  "bluz71/vim-nightfly-colors",
-  "rebelot/kanagawa.nvim",
-  "NLKNguyen/papercolor-theme",
-  "romainl/Apprentice",
-  -- configure lazyvim to load gruvbox
+  {
+    "crispybaccoon/evergarden",
+    opts = {
+      transparent_background = true,
+      contrast_dark = "medium", -- 'hard'|'medium'|'soft'
+      overrides = {}, -- add custom overrides
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "HoNamDuong/hybrid.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        -- Recommended - see "Configuring" below for more config options
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+      })
+    end,
+  },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  },
+  { "cryptomilk/nightcity.nvim", version = false },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "PaperColor",
-      -- apprentice, PaperColor, kanagawa, nightfly, gruvbox
+      colorscheme = "cyberdream",
     },
   },
 }
---
+++ dot_config/nvim/lua/plugins/overrides.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+}
