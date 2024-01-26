6cd1d179ffba56fe9dadd069a4d63129b98e6363
Eduardo Zamora <eduzambl@gmail.com>
2024-01-26 09:54:08 -0600

    feat: update wezterm and java nvim

--- dot_config/nvim/lua/plugins/jvm.lua
+++ dot_config/nvim/lua/plugins/jvm.lua
@@ -1,59 +1,25 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.java" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "graphql",
-        "groovy",
-        "kotlin",
-        "scala",
-      })
-    end,
-  },
-  {
+  "nvim-java/nvim-java",
+  dependencies = {
+    "nvim-java/lua-async-await",
+    "nvim-java/nvim-java-core",
+    "nvim-java/nvim-java-test",
+    "nvim-java/nvim-java-dap",
+    "MunifTanjim/nui.nvim",
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        gradle_ls = {},
-        graphql = {},
-        groovyls = {},
-        kotlin_language_server = {},
-      },
-      format = {
-        timeout_ms = 5000,
-      },
-    },
-  },
-  {
-    "williamboma/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "ktlint",
-      })
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      if type(opts.sources) == "table" then
-        local nls = require("null-ls")
-        vim.list_extend(opts.sources, {
-          -- kotlin
-          nls.builtins.diagnostics.ktlint,
-        })
-      end
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        kotlin = { "ktlint" },
-      },
-      format = {
-        timeout_ms = 5000,
+    "mfussenegger/nvim-dap",
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        registries = {
+          "github:nvim-java/mason-registry",
+          "github:mason-org/mason-registry",
+        },
       },
     },
   },
+  config = function()
+    require("java").setup({})
+    require("lspconfig").jdtls.setup({})
+  end,
 }
