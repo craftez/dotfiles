78623604f4db2f28648da9271cf3d3e0da21a2e6
Eduardo Zamora <eduzambl@gmail.com>
2024-03-22 11:34:48 -0600

    feat: update

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@ local opt = vim.opt
 
 opt.spelllang = "en_us"
 opt.spell = true
+opt.clipboard = "unnamedplus"
--- dot_config/nvim/lua/plugins/langs.lua
+++ dot_config/nvim/lua/plugins/langs.lua
@@ -1,6 +1,5 @@
 return {
   { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.docker" },
   { import = "lazyvim.plugins.extras.lang.elixir" },
   { import = "lazyvim.plugins.extras.lang.java" },
