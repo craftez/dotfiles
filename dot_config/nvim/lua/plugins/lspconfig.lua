ba818525b6d8930631a3b36cade194a94a3025c7
Eduardo Zamora <eduzambl@gmail.com>
2024-03-22 06:08:45 -0600

    update nvim

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -6,9 +6,8 @@ return {
     "neovim/nvim-lspconfig",
     version = false,
     opts = {
-      -- suppress virtual text
       diagnostics = {
-        virtual_text = false,
+        virtual_text = true,
       },
       -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
       servers = {
@@ -29,30 +28,23 @@ return {
           settings = {
             -- https://valentjn.github.io/ltex/settings.html
             ltex = {
-              language = "en-GB",
+              language = "en-US",
               additionalRules = {
                 enablePickyRules = true,
-                motherTongue = "fr",
-              },
-              -- https://community.languagetool.org/rule/list?lang=en
-              disabledRules = {
-                ["en-GB"] = { "TOO_LONG_SENTENCE", "OXFORD_SPELLING_Z_NOT_S", "DASH_RULE" },
-                fr = { "APOS_TYP", "FRENCH_WHITESPACE", "FR_SPELLING_RULE", "COMMA_PARENTHESIS_WHITESPACE" },
+                motherTongue = "es",
               },
             },
           },
         },
       },
       setup = {
-        -- integrate ltex_extra with lazyvim
-        -- https://github.com/LazyVim/LazyVim/discussions/403
         ltex = function(_, opts)
           vim.api.nvim_create_autocmd("LspAttach", {
             callback = function(args)
               local client = vim.lsp.get_client_by_id(args.data.client_id)
               if client.name == "ltex" then
                 require("ltex_extra").setup({
-                  load_langs = { "en-GB", "fr" }, -- languages for witch dictionaries will be loaded
+                  load_langs = { "en-US", "es" }, -- languages for witch dictionaries will be loaded
                   init_check = true, -- whether to load dictionaries on startup
                   path = vim.fn.stdpath("config") .. "/spell", -- path to store dictionaries.
                   log_level = "none", -- "none", "trace", "debug", "info", "warn", "error", "fatal"
