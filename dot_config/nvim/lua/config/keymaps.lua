e57cbf07b16c809626af6b805039fda09fb211d6
Eduardo Zamora <eduzambl@gmail.com>
2024-03-22 19:02:12 -0600

    feat: update keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
+default_options = { silent = true }
+expr_options = { expr = true, silent = true }
+
+-- paste over currently selected text without yanking it
+map("v", "p", '"_dP', default_options)
+
+-- Tab switch buffer
+map("n", "<TAB>", ":bnext<CR>", default_options)
+map("n", "<S-TAB>", ":bprevious<CR>", default_options)
+
+-- Cancel search highlighting with ESC
+map("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_options)
