407afc6a00298907391f17a962d1df58d3989491
Eduardo Zamora <eduzambl@gmail.com>
2025-04-15 17:29:32 -0600

    feat: remove folders

--- nvim/.config/nvim/lua/plugins/augment.lua
+++ nvim/.config/nvim/lua/plugins/augment.lua
@@ -1,11 +1,6 @@
 return {
   "augmentcode/augment.vim",
   config = function()
-    vim.g.augment_workspace_folders = {
-      "~/projects/orcish-hono/",
-      "~/Repos/vetspire",
-      "~/Repos/alle-frontend-provider-web/",
-      "~/Repos/domain-onboarding-service/",
-    }
+    vim.g.augment_workspace_folders = {}
   end,
 }
